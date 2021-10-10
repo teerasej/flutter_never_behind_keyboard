719d09edd157cbf992fd00cf0347502f5b58bda7
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-10-10 18:58:55 +0700

    Detect focus of TextField if focused, set NeverHideArea the value. This value'll be used to determine to scroll to NeverHideBottom or not.

--- lib/neverhide/never_hide_box.dart
+++ lib/neverhide/never_hide_box.dart
@@ -12,19 +12,6 @@ class NeverHideBox extends StatefulWidget {
 class _NeverHideBoxState extends State<NeverHideBox> {
   late NeverBehindKeyboardArea neverHideArea;
 
-  // focus after rendered
-  // @override
-  // void initState() {
-  //   //... your stuff
-
-  //   WidgetsBinding.instance?.addPostFrameCallback(_onLayoutDone);
-  //   super.initState();
-  // }
-
-  // _onLayoutDone(_) {
-  //   FocusScope.of(context).requestFocus((widget.child as TextField).focusNode);
-  // }
-
   @override
   Widget build(BuildContext context) {
     context.visitAncestorElements((element) {
@@ -34,36 +21,21 @@ class _NeverHideBoxState extends State<NeverHideBox> {
         neverHideArea = parentWidget;
         return false;
       }
-
       return true;
     });
 
-    if (widget.child is TextField || widget.child is TextFormField) {
-      if (widget.child is TextField) {
-        var textField = widget.child as TextField;
-        textField.focusNode?.addListener(_onFocusChange);
-        //textField.scrollPadding = EdgeInsets.all(100);
-      }
+    if (widget.child is TextField) {
+      var textField = widget.child as TextField;
+      textField.focusNode?.addListener(_onFocusChange);
     }
 
-    return Container(
-      child: widget.child,
-    );
+    return widget.child;
   }
 
   void _onFocusChange() {
-    if (widget.child is TextField) {
-      var textField = widget.child as TextField;
-      if (textField.focusNode!.hasFocus) {
-        // neverHideArea.focusToBottom();
-        neverHideArea.scrollToBottom();
-      }
+    var textField = widget.child as TextField;
+    if (textField.focusNode!.hasFocus) {
+      neverHideArea.widgetInFocus = true;
     }
-
-    // BuildContext currentContext =
-    //     (neverHideArea.key as GlobalKey).currentContext as BuildContext;
-    // Scrollable.ensureVisible(currentContext,
-    //     alignmentPolicy: ScrollPositionAlignmentPolicy.keepVisibleAtEnd,
-    //     alignment: 20);
   }
 }
